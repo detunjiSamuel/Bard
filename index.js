dd052d4d04189ca380ad73d00f156b2fd99bb1b0
Adetunji Samuel <samadetunji01@gmail.com>
2023-05-06 23:19:30 +0100

    remove cli usage

--- index.js
+++ index.js
@@ -1,20 +1,5 @@
 #!/usr/bin/env node
 
-require("dotenv").config();
-
-console.log("session : ", process.env.BARD_SESSION);
-
 const Bard = require("./src/bard.js");
 
 module.exports = Bard;
-
-if (require.main === module) {
-  console.log("CALLED FROM CLI");
-
-  const bard = new Bard(process.env.BARD_SESSION);
-  (async () => {
-
-   const result = await bard.ask("What is the meaning of life?");
-   console.log("Result:", result);
- })();
-}
