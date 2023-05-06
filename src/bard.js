0095727caa49a6d98945cde7252f0c5577eb22cc
Adetunji Samuel <samadetunji01@gmail.com>
2023-05-06 20:38:58 +0100

    add create session

--- src/bard.js
+++ src/bard.js
@@ -1,17 +1,98 @@
 class Bard {
+  constructor(sessionId) {
+    this.session = sessionId;
+    this.requestId = this.#requestId;
+    this.question = "what is the meaning of life?";
+    this.conversationId = "";
+    this.responseId = "";
+    this.choiceId = "";
 
- constructor(session)
- {
-    this.session =  session
+    this.session = this.createSession();
+  }
 
+
+
+  get #requestId() {
+    return "requestId";
+  }
+
+  get #requestParams() {
+    return {
+      bl: "boq_assistant-bard-web-server_20230419.00_p1",
+      _reqid: this.requestId,
+      rt: "c",
+    };
+  }
+
+  get #messageContent() {
+    let content = new Array(3);
+
+    content[0] = [this.question];
+
+    content[2] = [this.conversationId, this.responseId, this.choiceId];
+
+    return content;
+  }
+
+  get #requestUrl() {
+    let url =
+      "https://bard.google.com/_/BardChatUi/data/assistant.lamda.BardFrontendService/StreamGenerate";
+
+    const params = this.#requestParams;
+
+    for (const [key, value] of Object.entries(params)) {
+      url += `?${key}=${value}`;
+    }
+
+    return url;
+  }
+
+  createSession() {
+    session = {
+      headers: new Headers(),
+      credentials: "include",
+    };
+
+    let headers = {
+      Host: "bard.google.com",
+      "X-Same-Domain": "1",
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36",
+      "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+      Origin: "https://bard.google.com",
+      Referer: "https://bard.google.com/",
+    };
+
+    for (const [key, value] of Object.entries(headers)) {
+      session.headers.append(key, value);
+    }
+
+    return session;
+  }
+
+  async processQuestionRequest() {
+    data = {
+      "f.req": JSON.stringify([null, JSON.stringify(this.#messageContent)]),
+      at: "something",
+    };
+
+    try {
+      const response = await fetch(this.#requestUrl, {
+        method: "POST",
+        headers: this.session.headers,
+        credentials: thus.session.credentials,
+        body: JSON.stringify({ data }),
+      });
+    } catch (e) {
+      console.error("error occured in processQuestionRequest", e);
+    }
+  }
+
+
+  ask(question) {
+   return "answer";
  }
 
- ask(question)
- {
-  return 'answer'
- }
 }
 
-
-
-module.exports = Bard
+module.exports = Bard;
